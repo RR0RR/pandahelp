40bd6d6a8da98467c9af29c62e180c731ded9ed0
Qin Ding Han <s10180179@connect.np.edu.sg>
2018-10-31 22:49:16 +0800

    Student class

---
+++ pandahelp app/App_Code/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pandahelp_app
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int PhoneNo { get; set; }
+        public string description { get; set; }
+        public string photo { get; set; }
+        public string password { get; set; }
+        public string course { get; set; }
+    }
+}
